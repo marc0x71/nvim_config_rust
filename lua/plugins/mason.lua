7a25f6ed28306d27f55a07bfa4b1d715a677b02d
Marco Bollero <m.bollero@gmail.com>
2026-06-28 10:19:23 +0200

    Aggiunto CRATES e TAPLO per la gestione del Cargo.toml ed i file TOML in generale

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,14 +1,17 @@
 return {
-  "williamboman/mason.nvim",
+  "mason-org/mason.nvim",
   dependencies = {
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason-lspconfig.nvim",
     "jay-babu/mason-nvim-dap.nvim",
+    "neovim/nvim-lspconfig",
   },
   config = function()
     local mason = require("mason")
     local mason_lspconfig = require("mason-lspconfig")
     local mason_nvim_dap = require("mason-nvim-dap")
-    
+
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+
     -- Setup Mason
     mason.setup({
       ui = {
@@ -19,20 +22,36 @@ return {
         }
       }
     })
-    
+
     -- Setup Mason LSPConfig
     mason_lspconfig.setup({
       -- Questa lista è vuota perché per Rust usiamo rustaceanvim
       -- che gestisce rust-analyzer automaticamente
-      ensure_installed = {},
+      -- Aggiunto TAPLO per la gestione del Cargo.toml ed i file TOML in generale
+      ensure_installed = { "taplo" },
     })
-    
+
     -- Setup Mason DAP - installa automaticamente codelldb
     mason_nvim_dap.setup({
       ensure_installed = { "codelldb" },
       automatic_installation = true,
     })
-    
+
+    vim.lsp.config("taplo", {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      settings = {
+        evenBetterToml = {
+          schema = {
+            enabled = true,
+            associations = {
+              ["Cargo\\.toml$"] = "https://json.schemastore.org/cargo.json",
+            },
+          },
+        },
+      },
+    })
+
     -- Keymap per aprire Mason
     vim.keymap.set('n', '<leader>m', '<cmd>Mason<cr>', { desc = 'Open Mason' })
   end,
